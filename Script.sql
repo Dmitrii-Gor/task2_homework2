24dcf0b821b0725584ece04f3b65cea98c940944
Dmitrii Gor <dima.goryunenko@mail.ru>
2022-12-02 17:20:09 +0300

    Change Script

--- Script.sql
+++ Script.sql
@@ -1,35 +1,42 @@
 create table if not exists Genre (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL
+	name VARCHAR(40) not null
 );
 create table if not exists Musician (
 	id INTEGER PRIMARY KEY,
-	name VARCHAR(40) not NULL 
+	name VARCHAR(40) not null
 );
 create table if not exists Genre_Relation (
 	id SERIAL PRIMARY KEY,
-	Genre_id INTEGER NOT NULL REFERENCES Genre(id),
-	Musician_id INTEGER NOT NULL REFERENCES Musician(id)
+	Genre_id INTEGER not null REFERENCES Genre(id),
+	Musician_id INTEGER not null REFERENCES Musician(id)
  );
 create table if not exists Album (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR(40) not null,
-	year_of_issue INTEGER NOT NULL
+	year_of_issue INTEGER not null,
+	check (year_of_issue > 1900)
 );
 create table if not exists Album_Relation (
 	id SERIAL PRIMARY KEY,
-	Album_id INTEGER NOT NULL REFERENCES Album(id),
-	Musician_id INTEGER NOT NULL REFERENCES Musician(id)
+	Album_id INTEGER not null REFERENCES Album(id),
+	Musician_id INTEGER not null REFERENCES Musician(id)
  );
 create table if not exists Collection (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT null,
-	year_of_issue INTEGER NOT null
+	name VARCHAR(40) not null,
+	year_of_issue INTEGER not null,
+	check (year_of_issue > 1900)
 );
 create table if not exists Track (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT null,
-	duration INTEGER NOT null,
-	Album_id INTEGER NOT NULL REFERENCES Album(id),
-	Collection_id INTEGER NOT NULL REFERENCES Collection(id)
+	name VARCHAR(40) not null,
+	duration INTEGER not null,
+	check (duration < 600),
+	Album_id INTEGER NOT NULL REFERENCES Album(id)
 );
+create table if not exists Colletion_Track_Relation (
+	id SERIAL PRIMARY KEY,
+	Collection_id INTEGER not null references Collection(id),
+	Track_id INTEGER not null REFERENCES Track(id)
+ );
